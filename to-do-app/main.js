12eaee463ae48c788a9453fe21ba0ec660a91729
Dat Hoang <dthoang6@gmail.com>
2022-08-29 19:31:51 -0700

    using JavaScript to build to do app to learn the JavaScript with web browsers

---
+++ to-do-app/main.js
@@ -0,0 +1,13 @@
+let ourForm = document.getElementById("ourForm");
+let ourField = document.getElementById("ourField");
+let ourList = document.getElementById("ourList");
+
+ourForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  createItem(ourField.value);
+});
+
+function createItem(x) {
+  let ourHTML = `<li>${x}<button>Delete</button> </li>`;
+  ourList.insertAdjacentHTML("beforeend", ourHTML);
+}
